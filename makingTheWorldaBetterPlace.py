693c0bdb28d535e918ea2f482a64fe370d0491a9
Hussein Fuseini <hussein.fuseini@turntabl.io>
2025-01-22 14:44:11 +0000

    edit old file

--- makingTheWorldaBetterPlace.py
+++ makingTheWorldaBetterPlace.py
@@ -2,4 +2,3 @@ print("Sing with me; Lets make the world a better place")
 print("We are in a new world")
 print("New World!")
 print("New People!")
-secret_key = '865cssdfafd465KLAJANAHDFA45464ADLJAFA75DFsdadf'
